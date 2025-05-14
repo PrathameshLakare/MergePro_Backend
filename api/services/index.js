c46a7ab917304f285a6aa9e16ad11825b016543b
Prathamesh Lakare <prathameshlakare001@gmail.com>
2025-05-14 12:41:11 +0530

    feat: create middleware for cookies

---
+++ api/services/index.js
@@ -0,0 +1,12 @@
+function setSecureCookie(res, token) {
+  res.cookie("access_token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  return res;
+}
+
+module.exports = { setSecureCookie };
