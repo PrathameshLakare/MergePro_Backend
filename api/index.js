d1fb358e8b7c73e72811ab386049940b10a7a255
Prathamesh Lakare <prathameshlakare001@gmail.com>
2025-05-17 16:12:24 +0530

    feat: update cors origin

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const User = require("./model/user.model");
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.BACKEND_URL,
     credentials: true,
   })
 );
